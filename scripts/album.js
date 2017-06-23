12e5afb531ddd22b6ebfb2a0a46d90a3be422541
Josh <jiac.lee46@gmail.com>
2017-06-23 12:25:34 -0700

    Problem Fixed

--- scripts/album.js
+++ scripts/album.js
@@ -63,8 +63,15 @@ var setCurrentAlbum = function(album){
 var findParentByClassName = function(element, targetClass) {
   if (element) {
     var currentParent = element.parentElement;
-    while (currentParent.className !== targetClass && currentParent.className !== null) {
+    if (currentParent === null) {
+      console.log("No parent found");
+      return null;
+    }
+    while (currentParent.className !== targetClass && currentParent.className !== null && currentParent.parentElement) {
       currentParent = currentParent.parentElement;
+    }
+  if (currentParent.className !== targetClass) {
+      console.log("No parent found with that class name");
     }
     return currentParent;
   }
