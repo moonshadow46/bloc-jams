f77e16ab86743ce6d1ffc9ec285b7193d38b4817
Josh <jiac.lee46@gmail.com>
2017-07-10 09:07:36 -0700

    Assignment21 1st Try

--- scripts/album.js
+++ scripts/album.js
@@ -30,7 +30,7 @@ var createSongRow = function(songNumber, songName, songLength){
     '<tr class="album-view-song-item">'
   + ' <td class="song-item-number" data-song-number ="' + songNumber + '">' + songNumber + '</td>'
   + ' <td class="song-item-title">' + songName + '</td>'
-  + ' <td class="song-item-duration">' + songLength + '</td>'
+  + ' <td class="song-item-duration">' + filterTimeCode(songLength) + '</td>'
   + '</tr>'
   ;
 
@@ -164,6 +164,21 @@ var updatePlayerBarSong = function() {
   $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
 
   $('.main-controls .play-pause').html(playerBarPauseButton);
+};
+
+var setCurrentTimePlayerBar = function(totalTime) {
+  $('.current-time').text(filterTimeCode(currentTime));
+};
+
+var setTotalTimePlayerBar = function(totalTIme) {
+  $('.currently-playing .total-time').text(filterTimeCode(totalTime));
+};
+
+var filterTimeCode = function(seconds) {
+  var minutes = Math.floor(seconds / 60);
+  seconds = Math.floor(seconds % 60);
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  return minutes + ":" + seconds;
 };
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
